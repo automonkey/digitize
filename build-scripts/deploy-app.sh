d4f067d6de1bbf97be3bf38a4e3d9d5966430fe7
Will Benyon <git@benyon.io>
2018-02-11 21:31:54 +0000

    Invalidate CloudFront distribution after app deploy
    Ensure CloudFront serves latest app files.

--- build-scripts/deploy-app.sh
+++ build-scripts/deploy-app.sh
@@ -13,6 +13,7 @@ if [ "$ENV" != "prod" ]; then
 fi
 
 AWS_DEPLOYMENT_BUCKET=www.${ENV_URL_PART}digitize.benyon.io
+echo "Deploying to bucket '$AWS_DEPLOYMENT_BUCKET'"
 
 (
     cd app
@@ -21,3 +22,12 @@ AWS_DEPLOYMENT_BUCKET=www.${ENV_URL_PART}digitize.benyon.io
     npm run build
     aws s3 sync build s3://$AWS_DEPLOYMENT_BUCKET --delete
 )
+
+CLOUDFRONT_ORIGIN_ID=${ENV}.digitize.benyon.io
+CLOUDFRONT_DISTRIBUTION_ID=$(aws cloudfront list-distributions \
+    --query "DistributionList.Items[?contains(Origins.Items[*].Id, '$CLOUDFRONT_ORIGIN_ID')]|[0].Id" \
+    --output text \
+)
+
+echo "Invalidating CloudFront distribution with origin '$CLOUDFRONT_ORIGIN_ID' ($CLOUDFRONT_DISTRIBUTION_ID)"
+aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_DISTRIBUTION_ID --paths "/*"
