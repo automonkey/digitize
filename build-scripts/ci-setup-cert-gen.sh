932ff0c123bcf6da94cd048b3824c87f1311e4e2
Will Benyon <git@benyon.io>
2019-07-14 14:03:27 +0100

    Remove use of virtualenv for ci cert gen
    No need to use virtualenv... it's a throwaway instance.

--- build-scripts/ci-setup-cert-gen.sh
+++ build-scripts/ci-setup-cert-gen.sh
@@ -5,7 +5,4 @@ set -eo pipefail
 ./build-scripts/ci-install-awscli.sh
 ./build-scripts/ci-install-ansible.sh
 
-pip install virtualenv
-virtualenv python-virtualenv-ci-cert-gen
-source python-virtualenv-ci-cert-gen/bin/activate
 pip install pyopenssl
