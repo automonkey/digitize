a2ccd72455ff53ae7c9d28f181e50b0b9518148d
Will Benyon <git@benyon.io>
2020-01-03 22:26:45 +0000

    Update Gandi DNS API in generate-ssl-cert.yml
    It seems like the API has changed slightly. While the old API seems to
    still be there, switching to the new API means we can do away with
    having to remember the zone id... which I can't remember.
    
    Probably good to stay up to date with the documented APIs too!

--- build-scripts/generate-ssl-cert.sh
+++ build-scripts/generate-ssl-cert.sh
@@ -1,6 +1,5 @@
 if [ -z ${ENV+x} ]; then echo "ENV is unset"; exit 1; else echo "ENV is set ($ENV)"; fi
 if [ -z ${GANDI_API_KEY+x} ]; then echo "GANDI_API_KEY is unset"; exit 1; else echo "GANDI_API_KEY is set"; fi
-if [ -z ${GANDI_DNS_ZONE_ID+x} ]; then echo "GANDI_DNS_ZONE_ID is unset"; exit 1; else echo "GANDI_DNS_ZONE_ID is set"; fi
 
 source python-virtualenv-ci-cert-gen/bin/activate
 
@@ -9,5 +8,4 @@ ansible-playbook -c local \
   -i localhost, \
   -e env="'${ENV}'" \
   -e gandi_api_key="'${GANDI_API_KEY}'" \
-  -e gandi_dns_zone_id="'${GANDI_DNS_ZONE_ID}'" \
   build-scripts/generate-ssl-cert.yml
