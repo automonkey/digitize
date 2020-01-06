002a697f0310f0481b86f76c46994bf607cdd8f1
Will Benyon <git@benyon.io>
2020-01-06 16:46:30 +0000

    Fix issue with SNI on new Gandi LiveDNS APIs
    Used this as a reason to jump to Python 3.
    
    Needed to update build agent image to install Python3 and venv.
    
    Error was:
    Failed to validate the SSL certificate for api.gandi.net:443. Make sure your managed systems have a valid CA certificate installed. If the website serving the url uses SNI you need python >= 2.7.9 on your managed machine  (the python executable used (/root/project/python-virtualenv-ci-cert-gen/bin/python) is version: 2.7.6

--- build-scripts/ci-setup-cert-gen.sh
+++ build-scripts/ci-setup-cert-gen.sh
@@ -5,7 +5,8 @@ set -eo pipefail
 ./build-scripts/ci-install-awscli.sh
 ./build-scripts/ci-install-ansible.sh
 
-pip install virtualenv
-virtualenv python-virtualenv-ci-cert-gen
+apt-get update
+apt-get install --assume-yes python3.5-venv build-essential libssl-dev python3-dev
+python3.5 -m venv python-virtualenv-ci-cert-gen
 source python-virtualenv-ci-cert-gen/bin/activate
 pip install pyopenssl
