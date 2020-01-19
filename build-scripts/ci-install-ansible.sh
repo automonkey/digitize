39f9474fdbb99c198c33020153cc9c941222210d
Will Benyon <git@benyon.io>
2020-01-19 23:51:16 +0000

    Use circleci python prebuilt container for cert gen
    - Switch to using prebuilt container rather than insdtalling Pyhton
    manually.
    
    - Install Ansible through pip instead of apt.
    
    - Remove use of virtual env. Not sure this was ever of much benefit...
    seems unnecessary when running in a container.

--- build-scripts/ci-install-ansible.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -eo pipefail
-
-apt-get update
-apt-get install -y -qq software-properties-common
-apt-add-repository --yes ppa:ansible/ansible
-apt-get update
-apt-get install -y -qq ansible
