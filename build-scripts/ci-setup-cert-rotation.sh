e11e4b3fd3d21043956e3b8297d72adcb9b427ab
Will Benyon <git@benyon.io>
2020-01-19 23:27:19 +0000

    Use circleci python container rather than installing on base image
    Should be quicker, and simpler to track python versions.
    
    Since we no longer need to install python to be able to install AWS CLI,
    it feels unnecessary to use the ci-install-awscli script - instead we
    now just call pip install directly in the ci setup script (it needs to
    be installed with --user as the global Python package location is not
    owned by our ci user).

--- build-scripts/ci-setup-cert-rotation.sh
+++ build-scripts/ci-setup-cert-rotation.sh
@@ -2,15 +2,10 @@
 
 set -eo pipefail
 
-./build-scripts/ci-install-awscli.sh
-
-apt-get update
-apt-get install --assume-yes python3 python3-pip
-pip3 install --upgrade setuptools
+pip install --user awscli
 
 (
     cd cloudfront-cert-rotator;
     make install;
     echo "export PATH=$PATH:$(make getbinpath)" >> $BASH_ENV
 )
-
