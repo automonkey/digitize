4a602a1046529e9b69767f7701376cf8c2ae6b41
Will Benyon <git@benyon.io>
2022-06-06 12:09:16 +0100

    Replace use of querystring.parse
    querystring.parse is deprecated, and Webpack no longer includes polyfills by
    default.
    
    Use URLSearchParams instead.

--- app/src/DropboxAuthComponent.js
+++ app/src/DropboxAuthComponent.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router';
-import querystring from 'querystring';
 import paths from './paths';
 
 class DropboxAuthComponent extends Component {
 
   componentWillMount() {
-    const qs = querystring.parse(window.location.hash.substr(1));
-    localStorage.setItem('dropbox-token', qs['access_token']);
+    const qs = new URLSearchParams(window.location.hash.substr(1));
+    localStorage.setItem('dropbox-token', qs.get('access_token'));
   }
 
   render() {
